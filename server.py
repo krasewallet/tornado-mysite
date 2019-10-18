111387472b0d5eea535642b50e054dc839f2ad7f
zqwei <zqwei@iflytek.com>
2019-10-18 15:48:24 +0800

    调整为https访问方式

--- server.py
+++ server.py
@@ -3,14 +3,22 @@ from tornado.ioloop import IOLoop
 from tornado.httpserver import HTTPServer
 from tornado.routing import Rule,RuleRouter,PathMatches
 from app import MyApplication
+from pathlib import Path
+import os
+
+cert = Path(os.path.realpath(__file__)).parents[0]/'ssl/server.pem'
+key = Path(os.path.realpath(__file__)).parents[0]/'ssl/server.key'
 
 def main():
     IOLoop.configure('tornado.platform.asyncio.AsyncIOLoop')
     router = RuleRouter([
         Rule(PathMatches(r"/.*"),MyApplication())
     ])
-    server = HTTPServer(router)
-    server.listen(80)
+    server = HTTPServer(router,ssl_options={
+        "certfile": str(cert),
+        "keyfile": str(key),
+    })
+    server.listen(443)
     IOLoop.instance().start()
 
 if __name__ == "__main__":
